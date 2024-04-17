776f72de1391f63c0254952fc4b209f412ca4a30
falahh6 <falahsss900@gmail.com>
2024-04-17 21:42:20 +0530

    #2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
+    //fetch the todos once
     fetch("http://localhost:3000/todos").then(async function (res) {
       const json = await res.json();
       console.log(json);
