84f638c63ee29a10ae325ff4819e970d86c34c45
falahh6 <falahsss900@gmail.com>
2024-04-17 21:38:33 +0530

    #1 commit

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -5,14 +5,20 @@
    } 
    ]
 */
-export function Todos({todos}){
-    return <div>
-        {todos.map(function(todo){
-            return <div>
-                <h1>{todo.title}</h1>
-                <h2>{todo.description}</h2>
-                <button>{todo.completed == true ? "Completed" : "Complete it"}</button>
-            </div>
-        })}
+export function Todos({ todos }) {
+  return (
+    <div>
+      {todos.map(function (todo, index) {
+        return (
+          <div key={index}>
+            <h1>{todo.title}</h1>
+            <h2>{todo.description}</h2>
+            <button>
+              {todo.completed == true ? "Completed" : "Complete it"}
+            </button>
+          </div>
+        );
+      })}
     </div>
+  );
 }
