a7fdeab485d6d47142ce87e80144637e3a2c8b35
Mxhran-m <mehranmulla367@gmail.com>
2024-04-17 16:57:18 +0530

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+// mongo url
+// mongodb+srv://mehran:Qc0vRrDusynFQBZK@todo.eme7uag.mongodb.net/
+mongoose.connect("mongodb+srv://mehran:Qc0vRrDusynFQBZK@todo.eme7uag.mongodb.net/todo")
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model("todos", todoSchema);
+module.exports = {
+    todo
+}
